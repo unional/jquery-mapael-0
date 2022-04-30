bd2550b2b53bcbd970d96dc574e61b89f313799f
FeJW <jiajiewu1988@gmail.com>
2022-04-30 16:35:47 -0700

    workaround for raphael.js compatibility

--- index.js
+++ index.js
@@ -655,7 +655,10 @@ $.fn.mapael.setHover = function (paper, mapElem, textElem) {
 $.fn.mapael.elemHover = function (paper, mapElem, textElem) {
     mapElem.animate(mapElem.attrsHover, mapElem.attrsHover.animDuration);
     textElem && textElem.animate(textElem.attrsHover, textElem.attrsHover.animDuration);
-    paper.safari();
+    // workaround for older version of Raphael
+    if (typeof paper.safari === "function") { 
+        paper.safari();
+    }
 }
 
 /**
@@ -667,7 +670,10 @@ $.fn.mapael.elemHover = function (paper, mapElem, textElem) {
 $.fn.mapael.elemOut = function (paper, mapElem, textElem) {
     mapElem.animate(mapElem.originalAttrs, mapElem.attrsHover.animDuration);
     textElem && textElem.animate(textElem.originalAttrs, textElem.attrsHover.animDuration);
-    paper.safari();
+    // workaround for older version of Raphael
+    if (typeof paper.safari === "function") { 
+        paper.safari();
+    }
 };
 
 /**
